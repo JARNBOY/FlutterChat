94db68ba99f6de6d4ccd5fe3c270c8eef0d8bd0a
Papon <papon.s@lawyerplusgroup.com>
2024-03-07 13:17:59 +0700

    [Section 14 - 277] Adding a Splash Screen (Loading Screen) (Reviewed by Papon S.)

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_chat/screens/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_chat/screens/chat.dart';
+import 'package:flutter_chat/screens/splash.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -30,6 +31,9 @@ class App extends StatelessWidget {
       home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
           if (snapshot.hasData) {
             return const ChatScreen();
           } 
