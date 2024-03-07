c76accc02cbbd935b4d71a6efe4e905a2ed42797
Papon <papon.s@lawyerplusgroup.com>
2024-03-07 13:09:03 +0700

    [Section 14 - 276] Showing Different Screens Based On The Authentication State (Reviewed by Papon S.)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/screens/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_chat/screens/chat.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -25,7 +27,14 @@ class App extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 63, 17, 177)),
       ),
-      home: AuthScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, snapshot) {
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          } 
+          return AuthScreen();
+        }),
     );
   }
 }
