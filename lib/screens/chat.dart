a8969290e99770da97a109c0170ae94634d494bd
Papon <papon.s@lawyerplusgroup.com>
2024-03-08 16:50:06 +0700

    [Section 14 - 294]Requesting Permissions & Getting an Address Token (Reviewed by Papon S.)

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,10 +1,34 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/widgets/chat_messenger.dart';
 import 'package:flutter_chat/widgets/new_messenger.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  void setupPushNotification() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+
+    final token = await fcm.getToken();
+    print(token);
+
+  }
+
+  @override
+  void initState() async {
+    // TODO: implement initState
+    super.initState();
+
+    setupPushNotification();
+  }
 
   @override
   Widget build(BuildContext context) {
